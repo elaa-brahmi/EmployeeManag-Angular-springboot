e097c02bdee921239d863b5491a7a1f2d3f79433
elaa <elaa.brahmii@gmail.com>
2025-01-18 17:16:57 +0100

    created an instance of the employee to bind the fields to the form fields

--- frontend/frontend/src/app/employee/employee.component.ts
+++ frontend/frontend/src/app/employee/employee.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { employee } from '../employee.model';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent {
-
+export class EmployeeComponent implements OnInit {
+  employee:employee={
+    employeeId:0,
+    employeeName:'',
+    employeeAddress:'',
+    employeeContactNumber:'',
+    employeeDepartment:'',
+    employeeGender:'',
+    employeeSkills:''
+  };
+  constructor(){}
+  ngOnInit(): void {
+  }
 }
