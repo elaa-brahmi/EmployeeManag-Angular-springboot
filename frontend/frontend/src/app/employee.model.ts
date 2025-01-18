295cee86c1771efe09b02152ead7e0669dec2a70
elaa <elaa.brahmii@gmail.com>
2025-01-18 12:42:58 +0100

    created employee model

---
+++ frontend/frontend/src/app/employee.model.ts
@@ -0,0 +1,9 @@
+export interface employee{
+  employeeId:number;
+  employeeName:string;
+  employeeContactNumber:string;
+  employeeAddress:string;
+  employeeGender:string;
+  employeeDepartment:string;
+  employeeSkills:string;
+}
