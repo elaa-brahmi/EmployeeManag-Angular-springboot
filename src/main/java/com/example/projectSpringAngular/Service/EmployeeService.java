7138d6faed493b34547cfe118b8e347ef64b83b2
elaa <elaa.brahmii@gmail.com>
2025-01-20 12:31:13 +0100

    updated the service

--- src/main/java/com/example/projectSpringAngular/Service/EmployeeService.java
+++ src/main/java/com/example/projectSpringAngular/Service/EmployeeService.java
@@ -15,6 +15,7 @@ import java.util.List;
 
         public Employee saveEmployee(Employee employee) {
             return employeeDao.save(employee);
+
         }
 
         public List<Employee> getEmployees() {
