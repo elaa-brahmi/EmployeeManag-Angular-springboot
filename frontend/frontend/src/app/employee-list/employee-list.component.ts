70a5a014958ff0abd36b678fd150cb4fd11d8287
elaa <elaa.brahmii@gmail.com>
2025-01-20 21:15:46 +0100

    completed

--- frontend/frontend/src/app/employee-list/employee-list.component.ts
+++ frontend/frontend/src/app/employee-list/employee-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../employee.service';
 import { employee } from '../employee.model';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
@@ -11,11 +13,18 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class EmployeeListComponent implements OnInit {
   dateSource:employee[]=[];
   displayedColumns: string[] = ['employeeId', 'employeeName', 'employeeContactNumber', 'employeeAddress','employeeGender','employeeDepartment','employeeSkills','delete'];
-constructor( private employeeService:EmployeeService ){
+constructor( private employeeService:EmployeeService,private router:Router ){
   this.getEmployeeList();
 }
 ngOnInit(): void {
 
+}
+update(employeeId:number):void{
+  this.router.navigate(['/employee',{employeeId:employeeId}]);
+
+}
+getEmployeeById(employeeId:number):Observable<employee>{
+  return this.employeeService.getEmployeeById(employeeId);
 }
 deleteById(employeeId:number):void{
   console.log(employeeId);
