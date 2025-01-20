ec41d1bc0eee9defed0e6ef8aed55c72f662dce6
elaa <elaa.brahmii@gmail.com>
2025-01-20 21:14:10 +0100

    completed the routing module and added resolver to the employee component

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeResolver } from './employee-resolver';
 
 const routes: Routes = [
   {path:'header' , component: HeaderComponent},
   {path:'' , component: HomeComponent},
   {path:'employee-list' , component: EmployeeListComponent},
-
-  {path:'employee' , component: EmployeeComponent}
+  {path:'employee' , component: EmployeeComponent,resolve:{employee:EmployeeResolver}}
 
 ];
 
