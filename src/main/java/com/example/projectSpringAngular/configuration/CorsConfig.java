4c1037d8f1582e216bd650d1be849428439837f4
elaa <elaa.brahmii@gmail.com>
2025-01-20 12:30:06 +0100

    security config class for cros

--- src/main/java/com/example/projectSpringAngular/configuration/CorsConfig.java
+++ src/main/java/com/example/projectSpringAngular/configuration/CorsConfig.java
@@ -1,4 +1,24 @@
 package com.example.projectSpringAngular.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Adjust to match your endpoint paths
+                        .allowedOrigins("http://localhost:4200") // Your Angular app's URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
+
