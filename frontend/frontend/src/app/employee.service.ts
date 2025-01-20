596e804961dc8c41cb08094952b808b9836e2e65
elaa <elaa.brahmii@gmail.com>
2025-01-20 21:14:43 +0100

    completed the employee service

--- frontend/frontend/src/app/employee.service.ts
+++ frontend/frontend/src/app/employee.service.ts
@@ -14,11 +14,18 @@ export class EmployeeService {
   public saveEmployee(employee: employee): Observable<employee>{
    return  this.httpClient.post<employee>(`${this.api}/save/employees`,employee);
   }
+  public getEmployeeById(id:number):Observable<employee>{
+    return this.httpClient.get<employee>(`${this.api}/get/employee/${id}`);
+  }
   public getEmployees():Observable<employee[]>{
     return this.httpClient.get<employee []>(`${this.api}/get/employees`);
 
   }
   public deleteById(id:number){
     return  this.httpClient.delete(`${this.api}/delete/employee/${id}`);
+  }
+  public update(employee:employee){
+    return this.httpClient.put<employee>(`${this.api}/update/employee`,employee);
+
   }
 }
