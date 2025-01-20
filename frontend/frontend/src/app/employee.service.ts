540c743c1eeedb7c4115811a33930f5e54c05e5c
elaa <elaa.brahmii@gmail.com>
2025-01-20 12:29:30 +0100

    updated

--- frontend/frontend/src/app/employee.service.ts
+++ frontend/frontend/src/app/employee.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { employee } from './employee.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  api="http://localhost:8080/api"
+// we will have CORS error because the ports of the api and the url are not the same
+  public saveEmployee(employee: employee): Observable<employee>{
+   return  this.httpClient.post<employee>(`${this.api}/save/employees`,employee);
+  }
 }
